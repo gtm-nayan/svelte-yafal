4bf305f3e76518b1be95e01c24f15f1f22dda593
gtm-nayan <gtmnayan@gmail.com>
2022-03-30 20:56:40 +0545

    Fix account for missing plugins

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -3,7 +3,7 @@ const open_props = require('open-props');
 const postcss_jit_props = require('postcss-jit-props');
 
 const config = {
-	plugins: [autoprefixer, postcss_jit_props(open_props)],
+	plugins: [postcss_jit_props(open_props), autoprefixer],
 };
 
 module.exports = config;
