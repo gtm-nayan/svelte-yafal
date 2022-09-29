b252b96f4459b2ae3050b4f966192d29949fc00f
gtm-nayan <50981692+gtm-nayan@users.noreply.github.com>
2022-09-29 15:07:12 +0000

    Deploying to gh-pages from @ gtm-nayan/svelte-yafal@7d735efb0481e1f18935c3ca8cc42f0252e64f90 🚀

---
+++ _app/immutable/chunks/1-748aba72.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-b51d1922.js";export{t as component};
---
+++ _app/immutable/chunks/2-7f06b432.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-5e16c621.js";export{t as component};
